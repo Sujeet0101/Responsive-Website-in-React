232199ecdee0fa235a7d3e4382f438014c354059
Sujeet0101 <suj337raut@gmail.com>
2024-08-30 12:02:42 +0545

    update navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,43 @@
-import styles from './style'
+import styles from "./style";
+
+import {
+  Navbar,
+  Hero,
+  Stats,
+  Business,
+  Billing,
+  CardDeal,
+  Testimonials,
+  Clients,
+  CTA,
+  Footer,
+} from "./components";
 
 const App = () => (
   //div that wrapping our entire application
   <div className="bg-primary w-full overflow-hidden">
-    <div className={`${styles.padding}X ${styles.flexCenter}`}>
-      <div className={`${styles.boxWidth}`}>Navbar</div>
-
-      <div className={`bg-primary ${styles.flexStart}`}>
-        <div className={`${styles.boxWidth}`}>Hero</div>
+    <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+      <div className={`${styles.boxWidth}`}>
+        <Navbar />
       </div>
+    </div>
 
-      <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
-        <div className={`${styles.boxWidth}`}>
-          Stats 
-          Business 
-          Billing 
-          CardDeal 
-          Testimonials 
-          Clients 
-          CTA 
-          Footer
-        </div>
+    <div className={`bg-primary ${styles.flexStart}`}>
+      <div className={`${styles.boxWidth}`}>
+        <Hero />
+      </div>
+    </div>
+
+    <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+      <div className={`${styles.boxWidth}`}>
+        <Stats />
+        <Business />
+        <Billing />
+        <CardDeal />
+        <Testimonials />
+        <Clients />
+        <CTA />
+        <Footer />
       </div>
     </div>
   </div>
