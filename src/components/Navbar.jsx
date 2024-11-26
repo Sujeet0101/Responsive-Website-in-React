de8af67d03ff19adc95184b69d1397d2791d1ac0
Sujeet0101 <suj337raut@gmail.com>
2024-11-26 16:17:04 +0545

    update hero section

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,7 +10,7 @@ const Navbar = () => {
 
   return (
     <nav className="w-full flex py-6 justify-between items-center navbar">
-      <img src={logo} alt="hoobank" className="w-[124px] h-[32]px" />
+      <img src={logo} alt="hoobank" className="w-[124px] h-[32px]" />
 
       <ul
         className="list-none sm:flex hidden justify-end
